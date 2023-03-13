316698aadd68cc0a68807dd0b5e13f46d33ac663
lawrence <lawrenceanthony70@gmail.com>
2023-03-13 20:41:12 +0100

    finished all major designs remaining the portfolio work

--- main_portfolio/js/main.js
+++ main_portfolio/js/main.js
@@ -1,11 +1,3 @@
-/*!
- * Item: Kitzu
- * Description: Personal Portfolio Template
- * Author/Developer: Exill
- * Author/Developer URL: https://themeforest.net/user/exill
- * Version: v1.0.2
- * License: Themeforest Standard Licenses: https://themeforest.net/licenses
- */
 !(function (e) {
   "use strict";
   e(function () {
@@ -116,29 +108,29 @@
       })(),
       e(".home-area").hasClass("video-variant") && e("#homeVideo").YTPlayer();
     var a, t, i;
-    tns({
-      container: "#about .testimonials-section .my-slider",
-      items: 2,
-      gutter: 30,
-      responsive: {
-        0: {
-          items: 1,
-          gutter: 0,
-        },
-        768: {
-          items: 2,
-          gutter: 30,
-        },
-      },
-      preventScrollOnTouch: "auto",
-      slideBy: "page",
-      mouseDrag: !0,
-      swipeAngle: !1,
-      speed: 400,
-      controls: !1,
-      autoHeight: !0,
-      navPosition: "bottom",
-    });
+    // tns({
+    //   container: "#about .testimonials-section .my-slider",
+    //   items: 2,
+    //   gutter: 30,
+    //   responsive: {
+    //     0: {
+    //       items: 1,
+    //       gutter: 0,
+    //     },
+    //     768: {
+    //       items: 2,
+    //       gutter: 30,
+    //     },
+    //   },
+    //   preventScrollOnTouch: "auto",
+    //   slideBy: "page",
+    //   mouseDrag: !0,
+    //   swipeAngle: !1,
+    //   speed: 400,
+    //   controls: !1,
+    //   autoHeight: !0,
+    //   navPosition: "bottom",
+    // });
     (a = function () {
       e("#resume .skills-section .single-skill").each(function () {
         var a = Math.min(100, Math.max(0, e(this).data("percentage"))),
@@ -173,44 +165,70 @@
           });
       }),
       e("#contact .contact-section .contact-form").on("submit", function (a) {
-        var t = e(this),
-          i = t.find("#contact-submit"),
-          s = i.text(),
-          o = t.find(".contact-feedback");
-        a.preventDefault(),
-          i.html("Wait...").addClass("wait").prop("disabled", !0),
-          setTimeout(function () {
-            e.ajax({
-              url: t.attr("action"),
-              type: "POST",
-              data: t.serialize(),
-            }).done(function (e) {
-              "success" == e
-                ? (i.removeClass("wait").html("Success").addClass("success"),
-                  o
-                    .addClass("success")
-                    .html("Thank you for your message. It has been sent.")
-                    .fadeIn(200),
-                  setTimeout(function () {
-                    i.html(s).removeClass("success").prop("disabled", !1),
-                      o.fadeOut(200).removeClass("success").html("");
-                  }, 6e3),
-                  t[0].reset())
-                : (console.log(e),
-                  i.removeClass("wait").html("Error").addClass("error"),
-                  o
-                    .addClass("error")
-                    .html(
-                      "Server error! Please check your browser console log for more details."
-                    )
-                    .fadeIn(200),
-                  setTimeout(function () {
-                    i.html(s).removeClass("error").prop("disabled", !1),
-                      o.fadeOut(200).removeClass("error").html("");
-                  }, 6e3));
-            });
-          }, 1e3);
+        // var t = e(this),
+        //   i = t.find("#contact-submit"),
+        //   s = i.text(),
+        //   o = t.find(".contact-feedback");
+        // a.preventDefault(),
+        //   i.html("Wait...").addClass("wait").prop("disabled", !0),
+        //   setTimeout(function () {
+        //     e.ajax({
+        //       url: t.attr("action"),
+        //       type: "POST",
+        //       data: t.serialize(),
+        //     }).done(function (e) {
+        //       "success" == e
+        //         ? (i.removeClass("wait").html("Success").addClass("success"),
+        //           o
+        //             .addClass("success")
+        //             .html("Thank you for your message. It has been sent.")
+        //             .fadeIn(200),
+        //           setTimeout(function () {
+        //             i.html(s).removeClass("success").prop("disabled", !1),
+        //               o.fadeOut(200).removeClass("success").html("");
+        //           }, 6e3),
+        //           t[0].reset())
+        //         : (console.log(e),
+        //           i.removeClass("wait").html("Error").addClass("error"),
+        //           o
+        //             .addClass("error")
+        //             .html(
+        //               "Server error! Please check your browser console log for more details."
+        //             )
+        //             .fadeIn(200),
+        //           setTimeout(function () {
+        //             i.html(s).removeClass("error").prop("disabled", !1),
+        //               o.fadeOut(200).removeClass("error").html("");
+        //           }, 6e3));
+        //     });
+        //   }, 1e3);
       });
+
+    e("#contact-submit").on("click", function (event) {
+      event.preventDefault();
+      var contactName = e("#contact-name").val();
+      var contactEmail = e("#contact-email").val();
+      var message = e("#contact-message").val();
+      var message_encode = `https://wa.me/+2348046220065?text=Hi my name is ${contactName},Email:${contactEmail}, ${message}`;
+
+      console.log(contactName, contactEmail, message);
+
+      if (contactName === "" || contactEmail === "" || message === "") {
+        e(".contact-feedback")
+          .addClass("error")
+          .html("sorry make sure all fields are filled and try again")
+          .fadeIn(200);
+        setTimeout(function () {
+          e(".contact-feedback").fadeOut(200).removeClass("error").html("");
+        }, 5000);
+        return;
+      }
+
+      var sendelement = document.createElement("a");
+      sendelement.href = message_encode;
+      sendelement.setAttribute("target", "_blank");
+      sendelement.click();
+    });
   }),
     e(window).on("load", function () {
       e(".preloader-icon").fadeOut(400),
